63af5593da3c4e124820ade1bf0a1bdbfd626eca
caoliyuan <893929746@qq.com>
2017-03-10 13:22:09 +0800

    创建项目

---
+++ NIIT.BookStore3.Web/NIIT.BookStore3.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NIIT.BookStore3.Web.MvcApplication" Language="C#" %>
---
+++ NIIT.BookStore3.Web/NIIT.BookStore3.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NIIT.BookStore3.Web.Startup))]
+namespace NIIT.BookStore3.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
